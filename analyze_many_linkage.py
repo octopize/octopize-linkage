9c99a6819324333fa0178017da0c92462d9e796e
Olivier Regnier-Coudert <37863704+olivierabz@users.noreply.github.com>
2024-11-14 15:39:21 +0100

    fix: update many linkage scripts

--- analyze_many_linkage.py
+++ analyze_many_linkage.py
@@ -1,21 +1,32 @@
-"""Example of how to call the different bricks on the PRA dataset."""
+"""Script analyzing linkages performed under different settings for a given dataset."""
 
 import pandas as pd
 
-from post_linkage_metrics import generate_projection_plot, get_correlations, get_non_shared_var_projections, plot_correlations
-from pre_linkage_metrics import ImputeMethod, contribution_score, get_best_n_from_m_variables, get_unicity_score
-from linkage import Distance, link_datasets, LinkingAlgorithm
+from post_linkage_metrics import generate_projection_plot, get_correlations, get_non_shared_var_projections, get_reconstruction_score, plot_correlations
+from linkage import Distance, LinkingAlgorithm
 
+stats = {
+    "ava_ori": [],
+    "distance": [],
+    "linkage_algo": [],
+    "corr_diff_sum": [],
+    "reconstruction_score": []
+}
 
+# Load and prepare original data
+original_df = pd.read_csv("data/pra_2023.csv")
+# drop IDs and constant columns
+original_df = original_df.drop(columns=['household_number', 'survey_year', 'rowid', 'matricule'])
 
 should_be_categorical_columns_1 = ['nationality', 'place_birth', 'sex', 'province', 'household_duties', 'relation_to_activity1', 'relation_to_activity2']
 should_be_categorical_columns_2 = ['nationality', 'place_birth', 'sex', 'province', 'relationship', 'main_occupation', 'availability', 'search_work', 'search_reason', 'search_steps', 'search_method', 'main_activity', 'main_prof_situation' ,'main_sector' ,'contract_type']
-
-
+for col in should_be_categorical_columns_1:
+    original_df[col] = original_df[col].astype(object)
+for col in should_be_categorical_columns_2:
+    original_df[col] = original_df[col].astype(object)
 
 # Select common/shared columns
 shared_columns = ['sex', 'nationality', 'age', 'province', 'place_birth']
-
 
 # Load split data to get column divisions
 df1_avatars = pd.read_csv("data/pra_A_unshuffled_avatars.csv")
@@ -23,93 +34,47 @@ df2_avatars = pd.read_csv("data/pra_B_unshuffled_avatars.csv")
 columns1 = set(df1_avatars.columns) - set(shared_columns)
 columns2 = set(df2_avatars.columns) - set(shared_columns)
 
+LINK_ORI_AVA = ["original", "avatars"]
+linkage_algos = [LinkingAlgorithm.LSA] # [LinkingAlgorithm.LSA, LinkingAlgorithm.MIN_ORDER]
+distances = [Distance.GOWER, Distance.PROJECTION_DIST_ALL_SOURCES, Distance.ROW_ORDER, Distance.RANDOM]
+# distances = [Distance.GOWER, Distance.PROJECTION_DIST_FIRST_SOURCE, Distance.PROJECTION_DIST_SECOND_SOURCE, Distance.PROJECTION_DIST_ALL_SOURCES, Distance.ROW_ORDER, Distance.RANDOM]
 
+for ori_ava in LINK_ORI_AVA:
+        for linkage_algo in linkage_algos:
+            for distance in distances:
+                # Load and prepare linked data
+                linked_df = pd.read_csv(f"data/pra_linked_data__{ori_ava}__{linkage_algo.value}__{distance.value}.csv")
+                
+                for col in should_be_categorical_columns_1:
+                    linked_df[col] = linked_df[col].astype(object)
+                for col in should_be_categorical_columns_2:
+                    linked_df[col] = linked_df[col].astype(object)
 
-stats = {
-    "ava_ori": [],
-    "distance": [],
-    "linkage_algo": [],
-    "corr_diff_sum": []
-}
+                # Compute and store statistics
+                corr_records, corr_avatars, corr_diff = get_correlations(original_df, linked_df, list(columns1), list(columns2))
+                reconstruction_score = get_reconstruction_score(original_df, linked_df)
 
-#### Get statistics from original unified PRA data
-original_df = pd.read_csv("data/pra_2023.csv")
-# drop IDs and constant columns
-original_df = original_df.drop(columns=['household_number', 'survey_year', 'rowid', 'matricule'])
+                stats["ava_ori"].append(ori_ava)
+                stats["distance"].append(distance.value)
+                stats["linkage_algo"].append(linkage_algo.value)
+                stats["corr_diff_sum"].append(corr_diff.sum().sum())
+                stats["reconstruction_score"].append(reconstruction_score)
 
-for col in should_be_categorical_columns_1:
-    original_df[col] = original_df[col].astype(object)
-for col in should_be_categorical_columns_2:
-    original_df[col] = original_df[col].astype(object)
+                # Compute and store plots
+                plt = plot_correlations(corr_records, corr_avatars, title=f"{linkage_algo.value} / {distance.value}")
+                plt.savefig(f"data/pra_linked_data__avatar__{linkage_algo.value}__{distance.value}_correlations.png")
 
+                proj_original, proj_linked = get_non_shared_var_projections(original_df, linked_df, list(columns1), list(columns2))
+                plt = generate_projection_plot(proj_original, proj_linked, title=f"Projection of non-shared variables for {linkage_algo}/{distance}")
+                plt.savefig(f"data/pra_linked_data__avatar__{linkage_algo.value}__{distance.value}_non_shared_var_projections.png")
 
-stats["ava_ori"].append("original")
-stats["distance"].append("unlinked")
-stats["linkage_algo"].append("unlinked")
-stats["corr_diff_sum"].append(0)
-
-
-# #### Get statistics from anonymized unified PRA data
-# avatars_df = ...
-
-# stats["ava_ori"].append("avatars")
-# stats["distance"].append("unlinked")
-# stats["linkage_algo"].append("unlinked")
-
-
-# #### Get statistics from each original+linked PRA data
-# linked_original_df = ...
-
-# stats["ava_ori"].append("original")
-# stats["distance"].append(...)
-# stats["linkage_algo"].append(...)
-
-
-#### Get statistics from each anonymized+linked PRA data
-
-linkage_algos = [LinkingAlgorithm.LSA, LinkingAlgorithm.MIN_ORDER]
-distances = [Distance.GOWER, Distance.PROJECTION_DIST_FIRST_SOURCE, Distance.PROJECTION_DIST_SECOND_SOURCE, Distance.PROJECTION_DIST_ALL_SOURCES, Distance.ROW_ORDER, Distance.RANDOM]
-
-# linkage_algos = [LinkingAlgorithm.LSA]
-# distances = [Distance.PROJECTION_DIST_FIRST_SOURCE, Distance.PROJECTION_DIST_ALL_SOURCES, Distance.RANDOM]
-
-# linkage_algos = [LinkingAlgorithm.MIN_ORDER]
-# distances = [Distance.RANDOM]
-
-
-for linkage_algo in linkage_algos:
-    for distance in distances:
-        df = pd.read_csv(f"data/pra_linked_data__avatar__{linkage_algo.value}__{distance.value}.csv")
-        
-        for col in should_be_categorical_columns_1:
-            df[col] = df[col].astype(object)
-        for col in should_be_categorical_columns_2:
-            df[col] = df[col].astype(object)
-
-        corr_records, corr_avatars, corr_diff = get_correlations(original_df, df, list(columns1), list(columns2))
-
-        print(corr_avatars)
-        print(corr_records)
-        print('corr_diff.sum().sum():', corr_diff.sum().sum())
-
-
-        plt = plot_correlations(corr_records, corr_avatars, title=f"{linkage_algo.value} / {distance.value}")
-        plt.savefig(f"data/pra_linked_data__avatar__{linkage_algo.value}__{distance.value}_correlations.png")
-
-        # assert False
-
-
-        proj_original, proj_linked = get_non_shared_var_projections(original_df, df, list(columns1), list(columns2))
-        print('proj_original: ', proj_original)
-        print('proj_linked: ', proj_linked)
-
-        plt = generate_projection_plot(proj_original, proj_linked, title=f"Projection of non-shared variables for {linkage_algo}/{distance}")
-        plt.savefig(f"data/pra_linked_data__avatar__{linkage_algo.value}__{distance.value}_non_shared_var_projections.png")
-
-        stats["ava_ori"].append("avatars")
-        stats["distance"].append(distance.value)
-        stats["linkage_algo"].append(linkage_algo.value)
-        stats["corr_diff_sum"].append(corr_diff.sum().sum())
+                stats_df = pd.DataFrame(stats)
+                print(' ==== stats_df ====')
+                print(stats_df)
 
 stats_df = pd.DataFrame(stats)
+date = pd.Timestamp.now().strftime("%Y%m%d_%H%M%S")
+stats_df.to_csv(f"data/many_linkage_stats_{date}.csv", index=False)
+
+print(' ==== Final stats_df ====')
 print(stats_df)
--- run_many_linkage.py
+++ run_many_linkage.py
@@ -1,44 +1,45 @@
-"""Example of how to call the different bricks on the PRA dataset."""
+"""Script performing linkage under different settings for a given dataset."""
 
-import numpy as np
 import pandas as pd
 
-from pre_linkage_metrics import ImputeMethod, contribution_score, get_best_n_from_m_variables, get_unicity_score
 from linkage import Distance, link_datasets, LinkingAlgorithm
-
-
-# load avatars from both sources
-df1_avatars = pd.read_csv("data/pra_A_unshuffled_avatars.csv")
-df2_avatars = pd.read_csv("data/pra_B_unshuffled_avatars.csv")
-
-should_be_categorical_columns_1 = ['nationality', 'place_birth', 'sex', 'province', 'household_duties', 'relation_to_activity1', 'relation_to_activity2']
-should_be_categorical_columns_2 = ['nationality', 'place_birth', 'sex', 'province', 'relationship', 'main_occupation', 'availability', 'search_work', 'search_reason', 'search_steps', 'search_method', 'main_activity', 'main_prof_situation' ,'main_sector' ,'contract_type']
-
-for col in should_be_categorical_columns_1:
-    df1_avatars[col] = df1_avatars[col].astype(object)
-for col in should_be_categorical_columns_2:
-    df2_avatars[col] = df2_avatars[col].astype(object)
 
 # Select common/shared columns
 shared_columns = ['sex', 'nationality', 'age', 'province', 'place_birth']
 
 
-linkage_algos = [LinkingAlgorithm.LSA, LinkingAlgorithm.MIN_ORDER]
-# linkage_algos = [LinkingAlgorithm.LSA]
-distances = [Distance.GOWER, Distance.PROJECTION_DIST_FIRST_SOURCE, Distance.PROJECTION_DIST_SECOND_SOURCE, Distance.PROJECTION_DIST_ALL_SOURCES, Distance.ROW_ORDER, Distance.RANDOM]
-# distances = [Distance.PROJECTION_DIST_FIRST_SOURCE, Distance.PROJECTION_DIST_ALL_SOURCES, Distance.RANDOM]
-# linkage_algos = [LinkingAlgorithm.MIN_ORDER]
-# distances = [Distance.RANDOM]
+LINK_ORI_AVA = ["original", "avatars"]
+linkage_algos = [LinkingAlgorithm.LSA] # [LinkingAlgorithm.LSA, LinkingAlgorithm.MIN_ORDER]
+distances = [Distance.GOWER, Distance.PROJECTION_DIST_ALL_SOURCES, Distance.ROW_ORDER, Distance.RANDOM]
+# distances = [Distance.GOWER, Distance.PROJECTION_DIST_FIRST_SOURCE, Distance.PROJECTION_DIST_SECOND_SOURCE, Distance.PROJECTION_DIST_ALL_SOURCES, Distance.ROW_ORDER, Distance.RANDOM]
+
 should_shuffle_before_linkage = True
 
-for linkage_algo in linkage_algos:
-    for distance in distances:
-        _df1_avatars = df1_avatars.copy()
-        _df2_avatars = df2_avatars.copy()
-        if distance != Distance.ROW_ORDER and should_shuffle_before_linkage:
-            _df2_avatars = _df2_avatars.sample(frac=1).reset_index(drop=True)
-        print(f"\n\nRunning with linkage algorithm: {linkage_algo.value}, distance: {distance.value} ...")
-        # link the two sources
-        linked_df = link_datasets(_df1_avatars, _df2_avatars, shared_columns, distance=distance, linking_algo=linkage_algo)
-        linked_df.to_csv(f"data/pra_linked_data__avatar__{linkage_algo.value}__{distance.value}.csv", index=False)
-        # print("\n\nLinked data: \n", linked_df)
+for ori_ava in LINK_ORI_AVA:
+    if ori_ava == "avatars":
+        # load avatars from both sources
+        df1 = pd.read_csv("data/pra_A_unshuffled_avatars.csv")
+        df2 = pd.read_csv("data/pra_B_unshuffled_avatars.csv")
+    else:
+        df1 = pd.read_csv("data/pra_A.csv")
+        df2 = pd.read_csv("data/pra_B.csv")
+
+    should_be_categorical_columns_1 = ['nationality', 'place_birth', 'sex', 'province', 'household_duties', 'relation_to_activity1', 'relation_to_activity2']
+    should_be_categorical_columns_2 = ['nationality', 'place_birth', 'sex', 'province', 'relationship', 'main_occupation', 'availability', 'search_work', 'search_reason', 'search_steps', 'search_method', 'main_activity', 'main_prof_situation' ,'main_sector' ,'contract_type']
+
+    for col in should_be_categorical_columns_1:
+        df1[col] = df1[col].astype(object)
+    for col in should_be_categorical_columns_2:
+        df2[col] = df2[col].astype(object)
+
+    for linkage_algo in linkage_algos:
+        for distance in distances:
+            _df1 = df1.copy()
+            _df2 = df2.copy()
+            if distance != Distance.ROW_ORDER and should_shuffle_before_linkage:
+                _df2 = _df2.sample(frac=1).reset_index(drop=True)
+            
+            # link the two sources
+            print(f"\n\nRunning {ori_ava} with linkage algorithm: {linkage_algo.value}, distance: {distance.value} ...")
+            linked_df = link_datasets(_df1, _df2, shared_columns, distance=distance, linking_algo=linkage_algo)
+            linked_df.to_csv(f"data/pra_linked_data__{ori_ava}__{linkage_algo.value}__{distance.value}.csv", index=False)
